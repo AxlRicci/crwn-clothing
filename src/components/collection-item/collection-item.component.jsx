9783f95f5391225a244a0504fe979847486a1178
AxlRicci <alexbricci@gmail.com>
2020-08-10 23:20:45 -0400

    updated shop data with new data, updated cart utils to group items in cart reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
+
 
 import './collection-item.styles.scss';
 
 
-const CollectionItem = ({imageUrl, name, price}) => (
+const CollectionItem = ({item, addItem}) => {
+    const {imageUrl, name, price} = item
+    return (
     <div className="collection-item">
         <div className="image"
             style={{
@@ -14,7 +21,12 @@ const CollectionItem = ({imageUrl, name, price}) => (
             <span className="name">{name}</span>
             <span className="price">{price}</span>
         </div>
+        <CustomButton onClick={() => addItem(item)} inverted>Add to cart</CustomButton>
     </div>
-)
+)}
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,5 +1,6 @@
 const CartActionTypes = {
-    TOGGLE_CART_DROPDOWN: 'TOGGLE_CART_DROPDOWN'
+    TOGGLE_CART_DROPDOWN: 'TOGGLE_CART_DROPDOWN',
+    ADD_ITEM: 'ADD_ITEM'
 };
 
 export default CartActionTypes;
